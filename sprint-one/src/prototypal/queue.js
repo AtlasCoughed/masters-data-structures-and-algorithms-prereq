ec09416a8dd34f62dadda72aa697abb9a868aea8
MKS-students <mks.students@hackreactor.com>
2016-06-01 17:38:24 -0700

    Completed 2/4

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,36 @@
-var Queue = function() {
+var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  someInstance.storage = {};
+  someInstance.count = 0;
+
+  // Implement the methods below
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+queueMethods.dequeue = function(){
+  this.count && this.count--;
+  var result = this.storage["0"];
+  delete this.storage["0"];
+  var len =  Object.keys(this.storage).length;
+  for(var i = 0; i < len; i++) {
+    this.storage[i] = this.storage[(i+1)];
+  }
+  return result;
+};
+
+queueMethods.size = function(){
+  return this.count;
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function(){
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  // Implement the methods below
+  someInstance.storage = {};
+  someInstance.count = 0;
+
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+
+stackMethods = {};
+
+stackMethods.push = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+  console.log(this)
+};
+
+stackMethods.pop = function(){
+  this.count && this.count--;
+  var result = this.storage[this.count];
+  delete this.storage[this.count];
+  return result;
+};
+
+stackMethods.size = function(){
+  return this.count;
+};
 
 
+//assuming(extendsConstructor).expect(constructorPropertyCount).to.be.above(0);
